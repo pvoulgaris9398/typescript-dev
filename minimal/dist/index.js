92cc325b9a496941bc2429e7534f3a005a50bef3
Pete Voulgaris <petevoulgaris@gmail.com>
2026-02-23 17:41:50 -0500

    shuffled folders around

---
+++ minimal/dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(user) {
+    return `Hello, ${user.firstName} ${user.lastName}!`;
+}
+const person = {
+    firstName: "Josephine",
+    lastName: "Momma",
+};
+console.log(greet(person));
+//# sourceMappingURL=index.js.map
