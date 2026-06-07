7e0be62a9bdea2db8ce55a16490add30cd3b870d
Pete Voulgaris <petevoulgaris@gmail.com>
2026-06-07 16:56:26 -0400

    draft implementation

--- artemis/webapi/dist/index.js
+++ artemis/webapi/dist/index.js
@@ -1,23 +1,11 @@
-import { getRandomValues } from 'crypto';
-import express, { Request, Response } from 'express';
-
+import express from 'express';
 const app = express();
 const PORT = 3000;
-
-interface StockPriceListResponse {
-    ticker: string,
-}
-
-app.get('/ticker', (req: Request, res: Response<StockPriceListResponse>) => {
-
+app.get('/ticker', (req, res) => {
     const tickers = ["AAPL", "EV", "IBM", "HSBC", "MS", "GOOG", "AMZ", "EY", "MX"];
-
     const ticker = tickers[Math.floor(Math.random() * tickers.length)];
-
-    res.status(202).json({ ticker: ticker })
+    res.status(202).json({ ticker: ticker });
 });
-
 app.listen(PORT, () => {
     console.log('Server is listening at http://localhost:${PORT}');
 });
-
